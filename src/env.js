7dff9e074640cb707a1ec23ba6b2f704a0a4ad24
AlejoTorres2001 <torres.alejo2001@gmail.com>
2025-02-06 13:22:19 -0300

    update: env vars

--- src/env.js
+++ src/env.js
@@ -11,6 +11,8 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    NODE_VERSION: z.string().default("23.7.0"),
+    COREPACK_IGNORE_STRICT_SIGNATURES: z.number().default(1),
   },
 
   /**
@@ -29,6 +31,8 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    NODE_VERSION: process.env.NODE_VERSION,
+    COREPACK_IGNORE_STRICT_SIGNATURES: process.env.COREPACK_IGNORE_STRICT_SIGNATURES,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
